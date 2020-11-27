c68be0c07cd6c9d5133bdab06c90db7510c88db8
Elijah <elijahoneal@yahoo.com>
2020-11-26 23:20:00 -0500

    Mobile view + nav button

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const navSlide = () =>{
     const burger = document.querySelector('.burger-menu');
     const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links a');
     burger.addEventListener('click', () => {
         //Toggle Nav
     
@@ -12,6 +11,8 @@ const navSlide = () =>{
 
         //Burger Animation
         burger.classList.toggle('toggle');
+
+        console.log('print')
     });
 
     
